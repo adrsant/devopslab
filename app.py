b1870697dbdb2198ceb94b1da37e6339b2cbb7ba
adriano.santos <adrsant@gmail.com>
2021-05-03 21:42:20 -0300

    Recap

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def pagina_inicial():
     return "Laboratorio Pipeline DevOps"
-    break
+
 
 if __name__ == '__main__':
     app.run(debug=True)
