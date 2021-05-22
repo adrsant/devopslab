4c80e2049a664d461f75cbd614a2bad346e1016b
adriano.santos <adrsant@gmail.com>
2021-05-22 19:59:17 -0300

    Correção de configuração de CSRF

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask
 
-# from flask_wtf.csrf import CSRFProtect
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app)
 
 
-# csrf = CSRFProtect()
-# csrf.init_app(app)
-
-
-# @csrf.exempt
-@app.route('/')
+@app.route('/', methods=['GET'])
+@csrf.exempt
 def pagina_inicial():
     return "Hello World"
 
